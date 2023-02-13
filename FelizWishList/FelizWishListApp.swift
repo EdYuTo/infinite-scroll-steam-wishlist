c83563dcadc487106838cbe531c82f6b00a5d360
EdYuTo <edson.yudi.toma@gmail.com>
2023-02-12 22:24:32 -0300

    Initial Commit

---
+++ FelizWishList/FelizWishListApp.swift
@@ -0,0 +1,17 @@
+//
+//  FelizWishListApp.swift
+//  FelizWishList
+//
+//  Created by Edson Yudi Toma on 12/02/23.
+//
+
+import SwiftUI
+
+@main
+struct FelizWishListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
